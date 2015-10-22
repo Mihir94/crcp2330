a63948694152356a5f612fe04dcdce2bd242bd68
Mihir Srivastava <msrivastava@smu.edu>
2015-10-21 23:13:41 -0500

    Completed Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,29 +6,43 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-
+//add X, Y times and store in result R2
 	@R0
-	D=M
-	@x
-	M=D //value R0 in x
+	D=M 			//D has value of R0
+	@x				//variable x
+	M=D 			//x = R0 
+
 	@R1
 	D=M
 	@y
-	M=D //value R1 in y
-(LOOP)
-	@x
-	D=M //get value x
-	@y
-	D=M //get value y
+	M=D 
+
 	@R2
-	M=M+D //storing the value in memory location of 2
+	M=0 			//R2 = 0
+
+(LOOP)
 	@y
-	M=M-D
+	D=M
+	@END
+	D;JLE			//bug?
+
+	@x
+	D=M 			//D register has value of x
+
+	@R2
+	M=M+D 			//adding x to itself in R2 
+
+	@y
+	M=M-1 			//decrement y by 1 
+	D=M  
+
 	@LOOP
-	0;JEQ //check if 'y' is = to 0, otherwise loop
+	0;JMP 			//bug? JGT should work without above statement 
+
 //infinite loop
 (END)
 	@END
 	0;JMP
+
 
 
