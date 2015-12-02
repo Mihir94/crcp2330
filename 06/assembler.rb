74ad45ff4f017ac456d9b6c5e708cb184764151c
Mihir Srivastava <msrivastava@smu.edu>
2015-12-02 10:34:27 -0600

    improving file check code

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,11 +1,20 @@
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
-		File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
-  asm_filename = ARGV[0]
-  puts asm_filename
-else
+def args_valid?
+	ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
+end
+
+def file_readable?(path)
+	File.readable?(path)
+end
+
+unless args_valid?
 	abort("Usage: ./assembler.rb Prog.asm")
 end
 
+asm_filename = ARGV[0]
 
+unless file_readable?(asm_filename)
+	abort("#{asm_filename} not found or is unreadable.")
+end
 
+puts "The contents of #{asm_filename}"
